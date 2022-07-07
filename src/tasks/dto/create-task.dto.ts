d5a7f905329bdd09c2e012bf5b6e1b4ea968299c
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-07 01:14:11 +0000

    Validation pipes and class-validator

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
+
 // Data Transfer Object for creating new task parameters
 export class CreateTaskDto {
+	@IsNotEmpty()
 	title: string;
+	
+	@IsNotEmpty()
 	description: string;
 }
