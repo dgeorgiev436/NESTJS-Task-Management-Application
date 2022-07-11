06f040c5b54a452930ff99676e23baf0838fde09
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-11 23:50:40 +0000

    Create a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import {AuthCredentialsDto} from "./dto/auth-credentials.dto"
 
 // BOILER CODE
 @Injectable()
@@ -9,4 +10,8 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private usersRepository: UserRepository
     ) {}
+	
+	async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+		return this.usersRepository.createUser(authCredentialsDto);
+	}
 }
