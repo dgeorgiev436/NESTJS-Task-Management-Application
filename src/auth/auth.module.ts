033a9fdfdf3fe22c8b84b864be1fbae461e4262e
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-12 01:00:24 +0000

    Setting up JWT and Passport for authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import { PassportModule } from "@nestjs/passport"
+import {JwtModule} from "@nestjs/jwt"
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserRepository])
+    TypeOrmModule.forFeature([UserRepository]),
+	PassportModule.register({defaultStrategy: "jwt"}),
+	JwtModule.register({
+		secret: "topSecret51",
+		signOptions: {
+			expiresIn: 3600
+		}
+	})
   ],
   providers: [AuthService],
   controllers: [AuthController]
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
 import {AuthCredentialsDto} from "./dto/auth-credentials.dto"
+import * as bcrypt from "bcrypt"
+import { UnauthorizedException } from "@nestjs/common"
 
 // BOILER CODE
 @Injectable()
@@ -13,5 +15,16 @@ export class AuthService {
 	
 	async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
 		return this.usersRepository.createUser(authCredentialsDto);
+	}
+	
+	async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+		const { username, password } = authCredentialsDto;
+		const user = await this.usersRepository.findOne({username})
+		
+		if(user && (await bcrypt.compare(password, user.password))) {
+			return "success"
+		}else{
+			throw new UnauthorizedException("Invalid login credentials")
+		}
 	}
 }
