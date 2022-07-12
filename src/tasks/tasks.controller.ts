f8716838ad617f8799df87c2f81c0ed9efcc2dee
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-12 01:50:33 +0000

    Protecting the tasks controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Delete,
   Query,
+  UseGuards
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskStatus } from './task-status.enum';
@@ -14,8 +15,11 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from "@nestjs/passport"
 
 @Controller('tasks')
+// Protecting the whole controller with JWT token
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
