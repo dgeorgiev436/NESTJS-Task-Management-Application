589c09e91dc1a8718c6e1817383a21b2dcdc5923
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-07-14 00:11:25 +0000

    TypeOrm Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,31 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 
 @Module({
   imports: [
+// 	Use ConfigModule for Configuration
 	ConfigModule.forRoot({
 		envFilePath: [`.env.stage.${process.env.STAGE}`]
 	}),
     TasksModule,
-    // Object-relational mapping module ORM
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "task-management",
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+	TypeOrmModule.forRootAsync({ // Initialize the TypeOrmModule asynchronous
+		imports: [ConfigModule], // Depends on ConfigModule --> Wait for ConfigModule to finish initialization as it is a dependency
+		inject: [ConfigService], // Inject the ConfigService
+		useFactory: async (configService: ConfigService) => { // Dependency injection for configService 
+			return {
+				type: "postgres",
+				autoLoadEntities: true,
+				synchronize: true,
+				host: configService.get("DB_HOST"),
+				port: configService.get("DB_PORT"),
+				username: configService.get("DB_USERNAME"),
+				password: configService.get("DB_PASSWORD"),
+				database: configService.get("DB_DATABASE")
+			}
+		}
+	}),
     AuthModule
   ]
 })
